f3e08ab3744d9d3bb9fe3a9b6be70aceddd06780
sharika199 <chitturisharika@gmail.com>
2023-07-26 18:21:43 +0000

    Fistname

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const a = require("../country/state/city/index");
+const b = require("../utilities/utils/index");
+
+const getPeopleInCity = (c) => {
+  return b(c);
+};
+module.exports = getPeopleInCity;
